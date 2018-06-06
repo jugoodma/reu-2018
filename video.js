c21ffea4973bbcdf19817a1d7e1f14a0c337e99b
jugoodma <jugoodma@gmail.com>
2018-06-06 15:22:03 -0400

    more updates

--- video.js
+++ video.js
@@ -7,30 +7,28 @@ request.responseType = 'text'; // we are expecting JSON
 request.onload = function() {
     // this is what happens when we get a response
     // parse the JSON, then display the YT video
-    // the response is in: request.response;
     // we will update the display in this block
-    var start = 0.0;
-    var end = 10.0;
-    var id = 'M7lc1UVf-VE';
+    var res = JSON.parse(request.response); // example response: '{"id":"M7lc1UVf-VE","start":0.0,"end":10.0}'
 
+    // create the youtube video
     var tag = document.createElement('script');
     tag.src = "https://www.youtube.com/iframe_api";
     var firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
     var player;
     function onYouTubeIframeAPIReady() {
 	player = new YT.Player('player', {
 	    height: '390',
 	    width: '640',
-	    videoId: id,
+	    videoId: res.id,
+	    start: res.start,
+	    end: res.end,
 	    events: {
 		'onReady': onPlayerReady,
 		'onStateChange': onPlayerStateChange
 	    }
 	});
     }
-
-    player.seekTo(start,true);
+    player.seekTo(start,false);
 };
 request.send();
