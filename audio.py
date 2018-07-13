07150291992afd78829833a7ff6412e3ea25f02b
jugoodma <jugoodma@gmail.com>
2018-07-13 18:23:18 -0400

    dank

--- audio.py
+++ audio.py
@@ -2,11 +2,18 @@ import random
 import json
 import csv
 import math
+import requests
+import time
+from multiprocessing import Pool, TimeoutError
 from settings import *
 
+start = 0
+sub = 0
+end = 0
 msrvtt = 'videodatainfo_2017.json'
 audioset = 'unbalanced_train_segments.csv'
 sound = 'msr-vtt-sound.csv' # these are the videos in msr-vtt that have sound
+ytdirect = 'https://you-link.herokuapp.com/?url=https://www.youtube.com/watch?v='
 # number of videos per category to skip
 # started with 1 video per category
 # 7/3 running 10 videos per category
@@ -18,6 +25,8 @@ sound = 'msr-vtt-sound.csv' # these are the videos in msr-vtt that have sound
 cats = ['/m/09x0r', '/m/07yv9'] # 'Speech', 'Vehicle'
 
 print("Creating audio input from msr-vtt...")
+start = time.time()
+
 # number of vids per category
 # we have a total of 20 categories
 #num_vids = 10
@@ -26,6 +35,7 @@ print("Creating audio input from msr-vtt...")
 # random sampling - seed is for replicatability
 random.seed(6942069)
 
+print("Gathering msrvtt vids")
 # gather all videos with sound
 temp = []
 with open(data_path + sound, 'r', newline = '') as f:
@@ -39,8 +49,14 @@ videos = json.loads(open(data_path + msrvtt).read())['videos']
 for vid in videos:
     if vid['video_id'] in temp:
         ytid = vid['url'].split('=')[1]
-        output.append([ytid]+[math.ceil(vid['start time'])]+[math.floor(vid['end time'])])
+        output.append([ytid]+[vid['start time']]+[vid['end time']])
+random.shuffle(output)
+output = output[:5001]
 
+print(round(time.time() - start, 4))
+sub = time.time()
+
+print("Gathering audioset vids. These ids below are already contained in msrvtt")
 # add audioset videos
 temp = []
 slic = [output[i][0] for i in range(len(output))]
@@ -59,15 +75,37 @@ with open(data_path + audioset, 'r', newline = '') as f:
 
 # shuffle audioset vids
 random.shuffle(temp)
-while len(output) < 10000:
+while len(output) < 7000: # only enough money for 5000 videos :'(
     output.append(temp.pop(0))
 
+print(round(time.time() - sub, 4))
+sub = time.time()
+
+print("Translating ytid to json with direct video links")
+def manl(lst):
+    r = requests.get(ytdirect + lst[0])
+    return lst + [r.json()]
+
+# translate all youtube videos to direct json links
+with Pool(processes = 8) as pool:
+    output = list(pool.map(manl, output))
+
+# filter for errors
+output = list(filter(lambda lst: 'error' not in str(lst[3]), output))
+
+print(round(time.time() - sub, 4))
+sub = time.time()
+
+print("Writing final output file")
 # write final output file
 with open(data_path + environments['audio']['csv'], 'w', newline = '') as f:
     writer = csv.writer(f, quotechar = '"', delimiter = ',', quoting = csv.QUOTE_ALL, skipinitialspace = True)
     # write the header
-    writer.writerow(['ytid'] + ['start'] + ['end'])
+    writer.writerow(['ytid'] + ['start'] + ['end'] + ['json'])
     writer.writerows(output)
 
+print(round(time.time() - sub, 4))
+end = time.time()
+print("Total time: " + str(round(end - start, 4)))
 # done
 print("Done.")
