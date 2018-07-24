5290c9bc893f4279b65926cf29e3e783719f2b8e
jugoodma <jugoodma@gmail.com>
2018-07-24 16:47:55 -0400

    some updates

--- single-label.py
+++ single-label.py
@@ -16,22 +16,10 @@ final_data = environments['temporal']['csv']
 labels = {} # and descriptions
 usable_videos = {}
 # the amount of videos you want per category from the list of top 25 categories
-amount_per_category = 340 # 200
+amount_per_category = 200
 
 # set random seed for reproductability
 random.seed(6942069)
-
-# skip amount (started at 1)
-# 6/26 running 20 videos per category
-# 6/27 running 1 video per category
-# 6/28 running 4 video per category
-# 7/3 running 10 videos per category
-# 7/5 running 200 videos per category
-# 7/6 running 20 videos per category (new categories, some are the same)
-# skip = 240
-# changing to random shuffle
-
-print('Creating csv ' + final_data + ' with ' + str(interval) + 'sec intervals and ' + str(window) + 'sec window.')
 
 # the categories we want to run
 """
@@ -61,30 +49,22 @@ category_list = ['Child speech, kid speaking',
                  'Blender',
                  'Lawn mower']
 """
-category_list = ['Quack']
-print(len(category_list))
 
 # create dictionary for mapping obscured labels to human-readable labels + descriptions
 with open(data_path + ontology, 'r', encoding='latin-1') as json_data:
     data = json.load(json_data)
     for obj in data:
-        if obj['name'] in category_list:
+        if len(obj['child_ids']) == 0:
             labels[obj['id']] = {'name': obj['name'], 'description': obj['description'],}
-print(labels)
+            usable_videos[obj['name']] = []
 
-# comment
 with open(data_path + audioset, 'r', newline = '') as f:
     reader = csv.reader(f, quotechar = '"', delimiter = ',', quoting = csv.QUOTE_ALL, skipinitialspace = True)
-    # create usable_video datastructure
-    for i in range(len(category_list)):
-        x = []
-        usable_videos[category_list[i]] = x
     # skip the three header comments in AudioSet
     for j in range(3):
         next(reader)
     # check each AudioSet video for a few things
     # 1 - the number of labels for the video must be 1
-    # 2 - the label must be in our categories list
     for row in reader:
         google_labels = row[3].split(',')
         english_label = labels.get(google_labels[0])
@@ -92,25 +72,7 @@ with open(data_path + audioset, 'r', newline = '') as f:
             usable_videos[english_label['name']].append([row[0], row[1], row[2], english_label['name'], english_label['description']])
 
 # comment
-with open(data_path + final_data, 'w', newline = '') as f:
-    writer = csv.writer(f, quotechar = '"', delimiter = ',', quoting = csv.QUOTE_ALL, skipinitialspace = True)
-    writer.writerow(['ytid'] + ['start'] + ['end'] + ['label'] + ['description'])
-    # sample <amount_per_category> number of random videos from usable_videos
-    for key in usable_videos:
-        print(key)
-        print(len(usable_videos[key]))
-        # skip some
-        #for count in range(skip):
-        #    row = random.choice(usable_videos[key])
-        #    usable_videos[key].remove(row)
-        # shuffle usable_videos
-        random.shuffle(usable_videos[key])
-        # write videos
-        for count in range(amount_per_category):
-            row = usable_videos[key][count]
-            row[1] = int(float(row[1]))
-            row[2] = int(float(row[2]))
-            print(row)
-            writer.writerow(row)
-
-print('Done.')
+for key in usable_videos:
+    l = len(usable_videos[key])
+    if l >= 200:
+        print(key + ": " + str(l))
--- spatial.py
+++ spatial.py
@@ -22,18 +22,21 @@ with open(data_path + subset, 'r', newline = '') as f:
 #random.shuffle(temp)
 #temp = temp[0:20]
 starts = {}
+"""
 with open(data_path + audioset, 'r', newline = '') as f:
     reader = csv.reader(f, quotechar = '"', delimiter = ',', quoting = csv.QUOTE_ALL, skipinitialspace = True)
     for i in range(3):
         next(reader)
     for row in reader:
         starts[row[0]] = [int(float(row[1])), int(float(row[2]))] # start, end
+"""
 with open(data_path + final_data, 'w', newline = '') as f:
     writer = csv.writer(f,  quotechar = '"', delimiter = ',', quoting = csv.QUOTE_ALL, skipinitialspace = True)
     # write the header row
     writer.writerow(['ytid'] + ['start'] + ['end'] + ['label'])
     for row in temp:
-        offset = starts[row[1]][0]
+        #offset = starts[row[1]][0] # when using YouTube and not the actual .mp4 files
+        offset = 0
         for i in range(int(((float(row[4]) - float(row[3]) - window) / interval) + 1)): # calculate number of clips
             r = [row[1]] + [int((interval * i) + int(row[3])) + offset] + [int((interval * i) + window + int(row[3])) + offset] + [row[0]]
             print(r)
