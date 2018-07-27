dc204cc2739b56e6635dd3d341b289b492e54ed4
jugoodma <jugoodma@gmail.com>
2018-07-27 16:17:50 -0400

    describing a lot of processes

--- scripts/spatial-input.py
+++ scripts/spatial-input.py
@@ -1,41 +1,30 @@
 import csv
-import random
 from settings import *
 
 # begin
 interval = 1.0
 window = 1.0
 final_data = environments['spatial']['csv']
-audioset = 'unbalanced_train_segments.csv'
-subset = 'spatial-sub.csv'
+#audioset = 'unbalanced_train_segments.csv'
+# this subset file needs rows as follows:
+# [ label, youtube id, ave start time, ave end time ]
+# and no header
+# and separated by & not ,
+subset = input("subset file: ../data/")
 
 print('Creating csv ' + final_data + ' with ' + str(interval) + 'sec intervals and ' + str(window) + 'sec window.')
 
-random.seed(6942069)
-
-# create csv file with clips
-temp = []
+# put all rows from subset into temp list
 with open(data_path + subset, 'r', newline = '') as f:
     reader = csv.reader(f, quotechar = '"', delimiter = '&', quoting = csv.QUOTE_ALL, skipinitialspace = True)
-    for row in reader:
-        temp.append(row)
-#random.shuffle(temp)
-#temp = temp[0:20]
-starts = {}
-"""
-with open(data_path + audioset, 'r', newline = '') as f:
-    reader = csv.reader(f, quotechar = '"', delimiter = ',', quoting = csv.QUOTE_ALL, skipinitialspace = True)
-    for i in range(3):
-        next(reader)
-    for row in reader:
-        starts[row[0]] = [int(float(row[1])), int(float(row[2]))] # start, end
-"""
-with open(data_path + final_data, 'w', newline = '') as f:
+    temp = list(reader)
+
+# create mturk input csv
+with open(input_path + final_data, 'w', newline = '') as f:
     writer = csv.writer(f,  quotechar = '"', delimiter = ',', quoting = csv.QUOTE_ALL, skipinitialspace = True)
     # write the header row
     writer.writerow(['ytid'] + ['start'] + ['end'] + ['label'])
     for row in temp:
-        #offset = starts[row[1]][0] # when using YouTube and not the actual .mp4 files
         offset = 0
         for i in range(int(((float(row[4]) - float(row[3]) - window) / interval) + 1)): # calculate number of clips
             r = [row[1]] + [int((interval * i) + int(row[3])) + offset] + [int((interval * i) + window + int(row[3])) + offset] + [row[0]]
